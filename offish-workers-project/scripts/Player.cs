285e0c76cdb5db228598e06e92b920ba221bd7db
Henrybcool <henryfehlner5@gmail.com>
2025-09-26 17:20:45 -0400

    Refactored player.gd into Player.cs. Added acceleration to movement and added gamepad support

--- offish-workers-project/scripts/Player.cs
+++ offish-workers-project/scripts/Player.cs
@@ -1,16 +1,41 @@
 using Godot;
 using System;
 
-public partial class Player : Node
+public partial class Player : CharacterBody2D
 {
-	protected int speed = 400;
-	protected int accel = 5;
+	[Export]
+	protected int maxSpeed = 600;
 	
-	getInput();
+	[Export]
+	protected int acceleration = 10;
 	
-	public void getInput()
+	[Export]
+	protected int friction = 20;
+	
+	public override void _PhysicsProcess(double delta)
 	{
-		//vector2 direction = Input.get_vector("move_left", "move_right", "move_up", "move_down");
-		//print(direction);
+		// Called every frame. Delta is time since the last frame.
+		// Update game logic here.
+		movePlayer(delta);
+	}
+	
+	private void movePlayer(double delta)
+	{
+		// Get input
+		Vector2 moveDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		
+		// Apply acceleration
+		if (moveDirection != Vector2.Zero)
+		{
+			Velocity = Velocity.Lerp(moveDirection * maxSpeed, (float)delta * acceleration);
+		}
+		// Apply friciton
+		else
+		{
+			Velocity = Velocity.Lerp(Vector2.Zero, (float)delta * friction);
+		}
+		
+		//GD.Print(Velocity);
+		MoveAndSlide();
 	}
 }
