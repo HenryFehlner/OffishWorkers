e5360c5cd0cc6ecbc4fa2f24a16263e125081b60
atommy126 <93847765+atommy126@users.noreply.github.com>
2025-09-27 19:07:01 -0400

    Added basic attack functionality
    - Created AttackHitbox class for the creation of temporary hitboxes for attacks
    - Created Enemy class to act as a base for other enemy types
    - Added a simple enemy to the scene that can be hit around
    
    Bug: When the player is moving up and collides with the bottom of the enemy, the enemy sticks to the player

---
+++ offish-workers-project/scripts/AttackHitbox.cs
@@ -0,0 +1,149 @@
+using Godot;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public enum Targets { EnemiesOnly, PlayerOnly, AllButParent, All }
+
+public partial class AttackHitbox : Area2D
+{
+    //config stats
+    //parent node
+    protected Node parentNode;
+    //where to place relative to parent node
+    protected int damage;
+    //how long the hitbox is active for
+    protected float duration;
+    //what kind of entities can be hit
+    protected Targets affectsTargets;
+
+    //other fields
+    //collection of nodes already hit by this hitbox
+    protected HashSet<Node> hitNodes = new HashSet<Node>();
+    //knockback impulse
+    protected Vector2 knockbackImpulse;
+
+    /// <summary>
+    /// Creation method
+    /// </summary>
+    /// <param name="parent">Node the attack originates from</param>
+    /// <param name="config">Attack config data</param>
+    /// <returns>AttackHitbox for the attack</returns>
+    public static AttackHitbox Create(AttackHitboxConfig config)
+    {
+        AttackHitbox hitbox = new AttackHitbox();
+        hitbox.Configure(config);
+        return hitbox;
+    }
+    /// <summary>
+    /// Configure the hitbox based on config info passed in
+    /// </summary>
+    /// <param name="config">Configuration struct</param>
+    public void Configure(AttackHitboxConfig config)
+    {
+        //set field data
+        parentNode = config.ParentNode;
+        parentNode.AddChild(this);//could probably use this and take node param out of create method
+        damage = config.Damage;
+        duration = config.Duration;
+        affectsTargets = config.AffectsTargets;
+
+        //calculate knockback impulse
+        knockbackImpulse = config.KnockbackDirection * config.KnockbackStength;
+
+        //add collision shape
+        AddChild(new CollisionShape2D { Shape = (Shape2D)config.Shape.Duplicate(true) });
+
+        //layer
+        if (parentNode.IsInGroup("player"))
+        {
+            CollisionLayer = Layers.Bit(Layers.PLAYER_ATTACKS);
+        }
+        else
+        {
+            CollisionLayer = Layers.Bit(Layers.ENEMY_ATTACKS);
+        }
+
+        //masks
+        CollisionMask = affectsTargets switch
+        {
+            Targets.PlayerOnly => Layers.Bit(Layers.PLAYER),
+            Targets.EnemiesOnly => Layers.Bit(Layers.ENEMIES),
+            _ => Layers.Bit(Layers.PLAYER) | Layers.Bit(Layers.ENEMIES),
+        };
+
+        //position and rotation
+        Rotation = config.HitboxDirection.Angle();
+        Position = config.LocalOffset.Rotated(Rotation);
+
+
+        //signals
+        BodyEntered += OnBodyEntered;
+        AreaEntered += OnAreaEntered;
+        Monitoring = true;
+    }
+
+    private void OnBodyEntered(Node body) => TryHit(body);
+    private void OnAreaEntered(Area2D area) => TryHit(area);
+
+    /// <summary>
+    /// Attempt to apply a hit to an object
+    /// </summary>
+    /// <param name="target">target of the attack</param>
+    private void TryHit(Node target)
+    {
+        GD.Print($"Trying hit on {target}");
+        //return if target is null, OR target is the parent and the parent should not be able to be hit by the attack, OR this node has already been hit
+        if (target == null || (target == parentNode && affectsTargets != Targets.All) || hitNodes.Contains(target))
+        {
+            return;
+        }
+
+        bool targetIsPlayer = target.IsInGroup("player");//NEED TO SET UP GROUPS!!!!!!!!!!!!!!!!!!
+        bool targetIsEnemy = target.IsInGroup("enemies");
+
+        //check to see if target is able to be hit by this attack
+        if ((targetIsPlayer && affectsTargets == Targets.EnemiesOnly) || (targetIsEnemy && affectsTargets == Targets.PlayerOnly))
+        {
+            return;
+        }
+
+        //target is hit by attack
+        if (target.HasMethod("TakeHit"))
+        {
+            //call TakeHit method
+            target.Call("TakeHit", damage, knockbackImpulse, parentNode);
+        }
+
+    }
+
+    public async Task Run()
+    {
+        //timer
+        SceneTreeTimer timer = GetTree().CreateTimer(duration);
+        await ToSignal(timer, SceneTreeTimer.SignalName.Timeout);
+        QueueFree();
+    }
+
+}
+
+public struct AttackHitboxConfig
+{
+    //parent node
+    public Node ParentNode;
+    //where to place relative to parent node
+    public Vector2 LocalOffset;
+    //the direction the hitbox is facing
+    public Vector2 HitboxDirection;
+    //how much damage is inflicted
+    public int Damage;
+    //how long the hitbox is active for
+    public float Duration;
+    //shape of the hitbox
+    public Shape2D Shape;
+    //direction of knockback
+    public Vector2 KnockbackDirection;
+    //strength of knockback
+    public float KnockbackStength;
+    //what kind of entities can be hit
+    public Targets AffectsTargets;
+}
--- offish-workers-project/scripts/Player.cs
+++ offish-workers-project/scripts/Player.cs
@@ -1,41 +1,129 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
 public partial class Player : CharacterBody2D
 {
-	[Export]
-	protected int maxSpeed = 600;
-	
-	[Export]
-	protected int acceleration = 10;
-	
-	[Export]
-	protected int friction = 20;
-	
+	//movement
+	[Export] protected int maxSpeed = 600;
+	[Export] protected int acceleration = 10;
+	[Export] protected int friction = 20;
+	//stats
+	[Export] protected int maxHp = 100;
+    protected int currentHp;
+	//primary attack
+	[Export] protected int primaryDamage = 1;
+	[Export] protected float primaryDuration = .4f;
+	[Export] protected float primaryKnockbackAmount = 4000;
+
+
+	private bool isAttacking = false;
+	private Vector2 facingDirection = Vector2.Right; //default value so player can never face Vector2.zero
+
+	public override void _Ready()
+	{
+		base._Ready();
+		//add to group for registering attacks
+		AddToGroup("player");
+		//set collision layer and masks
+        CollisionLayer = Layers.Bit(Layers.PLAYER);
+        CollisionMask = Layers.Bit(Layers.ENVIRONMENT) | Layers.Bit(Layers.ENEMIES) | Layers.Bit(Layers.ENEMY_ATTACKS);
+
+		currentHp = maxHp;
+	}
+
+
 	public override void _PhysicsProcess(double delta)
 	{
 		// Called every frame. Delta is time since the last frame.
 		// Update game logic here.
-		movePlayer(delta);
+
+		//check for attacks
+		if (Input.IsActionJustPressed("primary_attack"))
+		{
+			_ = PrimaryAttack();
+		}
+
+		//movement
+		MovePlayer(delta);
 	}
-	
-	private void movePlayer(double delta)
+
+	private void MovePlayer(double delta)
 	{
 		// Get input
 		Vector2 moveDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-		
+
 		// Apply acceleration
-		if (moveDirection != Vector2.Zero)
+		if (moveDirection != Vector2.Zero && !isAttacking)
 		{
 			Velocity = Velocity.Lerp(moveDirection * maxSpeed, (float)delta * acceleration);
+			facingDirection = moveDirection.Normalized();
 		}
 		// Apply friciton
 		else
 		{
 			Velocity = Velocity.Lerp(Vector2.Zero, (float)delta * friction);
 		}
-		
+
 		//GD.Print(Velocity);
 		MoveAndSlide();
 	}
+
+	private async Task PrimaryAttack()
+	{
+		if (isAttacking)
+		{
+			return;
+		}
+		isAttacking = true;
+
+		//to have attacking move the player, add an impulse here
+
+		RectangleShape2D rect = new RectangleShape2D();
+		rect.Size = new Vector2(25, 15);
+
+		AttackHitbox hitbox = AttackHitbox.Create(new AttackHitboxConfig
+		{
+			ParentNode = this,
+			LocalOffset = new Vector2(15, 0),
+			HitboxDirection = facingDirection,
+			Damage = primaryDamage,
+			Duration = primaryDuration,
+			Shape = rect,
+			KnockbackDirection = facingDirection,
+			KnockbackStength = primaryKnockbackAmount,
+			AffectsTargets = Targets.EnemiesOnly,
+		});
+
+		//keep alive for duration
+		await hitbox.Run();
+
+		//attack end delay
+		//await ToSignal(GetTree().CreateTimer(0.8f), SceneTreeTimer.SignalName.Timeout);
+
+		//end attack
+		isAttacking = false;
+	}
+
+	/// <summary>
+	/// Take a hit from an attack, receiving damage and knockback
+	/// </summary>
+	/// <param name="damage">damage</param>
+	/// <param name="impulse">knockback force</param>
+	/// <param name="attacker">source of the attack</param>
+	public void TakeHit(int damage, Vector2 impulse, Node attacker)
+	{
+		//prevent negative damage from healing
+		Math.Max(damage, 0);
+		//reduce health
+		currentHp -= damage;
+		//apply knockback
+		Velocity += impulse;
+
+		//check for death
+		if (currentHp <= 0)
+		{
+			//player dies and enters some game over state
+		}
+    }
 }
