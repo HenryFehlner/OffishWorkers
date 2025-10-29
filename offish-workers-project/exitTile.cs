2c612ea0ce6585012cdbfec47475af1ce540812f
Emma Wood <ekwood0825@Gmail.com>
2025-10-29 12:04:48 -0400

    Added exit tiles

---
+++ offish-workers-project/exitTile.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class exitTile : CollisionShape2D
+{
+	[Signal] public delegate void EndTileReachedEventHandler();
+	
+	private GameplayController gameplayControllerScript; 
+	
+	public override void _Ready()
+	{
+		if (gameplayControllerScript == null)
+		{
+		gameplayControllerScript = GetNode("../../Gameplay Controller") as GameplayController;
+		}
+	}
+		
+	//Sends the end tile event reached signal once the player enters it
+	private void OnBodyEntered(Node body)
+	{				
+		if (body is CharacterBody2D player)
+		{
+			
+		}
+	}
+	
+	private void ChangeLevel()
+	{
+		
+	}
+}
