d08658ad2ef98f583513cb85f43ec9b754f65213
Henrybcool <henryfehlner5@gmail.com>
2025-11-19 12:54:21 -0500

    It compiles now

--- offish-workers-project/scripts/Layers.cs
+++ offish-workers-project/scripts/Layers.cs
@@ -3,12 +3,13 @@ using System;
 
 public static class Layers
 {
-    public const int ENVIRONMENT = 0;
-    public const int PLAYER = 1;
-    public const int ENEMIES = 2;
-    public const int PLAYER_ATTACKS = 3;
-    public const int ENEMY_ATTACKS = 4;
-    public const int DODGE = 5;
+	public const int ENVIRONMENT = 0;
+	public const int PLAYER = 1;
+	public const int ENEMIES = 2;
+	public const int PLAYER_ATTACKS = 3;
+	public const int ENEMY_ATTACKS = 4;
+	public const int DODGE = 5;
+	public const int DOORS = 6;
 
-    public static uint Bit(int layerIndex) => 1u << layerIndex;
+	public static uint Bit(int layerIndex) => 1u << layerIndex;
 }
